2944c9fbbf59ff91588b47491777da0887b72d29
Pedroca12345 <pepeplays8@gmail.com>
2025-04-21 14:19:41 -0300

    Capturando a tarefa interrompida

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -61,6 +61,12 @@ export function MainForm() {
         activeTask: null,
         secondsRemaining: 0, // Conferir depois
         formattedSecondsRemaining: '00:00',
+        tasks: prevState.tasks.map(task => {
+          if (prevState.activeTask && prevState.activeTask.id === task.id) {
+            return { ...task, interruptedDate: Date.now() };
+          }
+          return task;
+        })
       };
     });
   }
